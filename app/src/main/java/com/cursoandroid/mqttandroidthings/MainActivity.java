ebf7db3efe0beb4031d128421b38c8d045d5eaae
william_ST <wsulcata@everis.com>
2019-07-13 13:28:23 -0500

    Suscripción para control remoto del LED

--- app/src/main/java/com/cursoandroid/mqttandroidthings/MainActivity.java
+++ app/src/main/java/com/cursoandroid/mqttandroidthings/MainActivity.java
@@ -9,7 +9,10 @@ import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.GpioCallback;
 import com.google.android.things.pio.PeripheralManager;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
@@ -36,8 +39,9 @@ import java.util.ArrayList;
  *
  * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements MqttCallback {
 
+    /*
     private static final String TAG = "Things";
     private static final String topic = "WilliamST/boton";
     private static final String hello = "Hello world! Android Things conectada.";
@@ -49,10 +53,50 @@ public class MainActivity extends Activity {
     private final String PIN_BUTTON = "BCM23";
     private Gpio mButtonGpio;
     private MqttClient client;
+    */
+    private static final String TAG = "Things";
+    private final String PIN_LED = "BCM18";
+    public Gpio mLedGpio;
+    private static final String topic_gestion = "WilliamST/gestion";
+    private static final String topic_led = "WilliamST/led";
+    static final String hello = "Hello world! Android Things conectada.";
+    private static final int qos = 1;
+    private static final String broker = "tcp://broker.hivemq.com:1883";
+    MqttClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        PeripheralManager service = PeripheralManager.getInstance();
+        try {
+            mLedGpio = service.openGpio(PIN_LED);
+            mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+        } catch (IOException e) {
+            Log.e(TAG, "Error en el API PeripheralIO", e);
+        }
+        try {
+            String clientId = MqttClient.generateClientId();
+            client = new MqttClient(broker, clientId, new MemoryPersistence());
+            client.setCallback(this);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            connOpts.setKeepAliveInterval(60);
+            connOpts.setWill(topic_gestion, "Android Things desconectada!"
+                    .getBytes(), qos, false);
+            Log.i(TAG, "Conectando al broker " + broker);
+            client.connect(connOpts);
+            Log.i(TAG, "Conectado");
+            Log.i(TAG, "Publicando mensaje: " + hello);
+            MqttMessage message = new MqttMessage(hello.getBytes());
+            message.setQos(qos);
+            client.publish(topic_gestion, message);
+            Log.i(TAG, "Mensaje publicado");
+            client.subscribe(topic_led, qos);
+            Log.i(TAG, "Suscrito a " + topic_led);
+        } catch (MqttException e) {
+            Log.e(TAG, "Error en MQTT.", e);
+        }
+        /*
         try {
             PeripheralManager service = PeripheralManager.getInstance();
             mButtonGpio = service.openGpio(PIN_BUTTON);
@@ -75,43 +119,44 @@ public class MainActivity extends Activity {
         } catch (IOException e) {
             Log.e(TAG, "Error en PeripheralIO API", e);
         }
+        */
     }
-
-    private GpioCallback mCallback = new GpioCallback() {
-        @Override
-        public boolean onGpioEdge(Gpio gpio) {
-            try {
-                Log.i(TAG, "Botón pulsado!");
-                Log.i(TAG, "Publicando mensaje: " + click);
-                MqttMessage message = new MqttMessage(click.getBytes());
-                message.setQos(qos);
-                client.publish(topic, message);
-                Log.i(TAG, "Mensaje publicado");
-            } catch (MqttException e) {
-                e.printStackTrace();
-            }
-            return true;
-        }
-    };
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (mButtonGpio != null) {
-            mButtonGpio.unregisterGpioCallback(mCallback);
-            try {
-                mButtonGpio.close();
-            } catch (IOException e) {
-                Log.e(TAG, "Error en PeripheralIO API", e);
-            }
-        }
-        if (client != null) {
-            try {
+        try {
+            if (client != null && client.isConnected()) {
                 client.disconnect();
-            } catch (MqttException e) {
-                e.printStackTrace();
+            }
+        } catch (MqttException e) {
+            Log.e(TAG, "Error en MQTT.", e);
+        }
+        if (mLedGpio != null) {
+            try {
+                mLedGpio.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error en el API PeripheralIO", e);
+            } finally {
+                mLedGpio = null;
             }
         }
     }
 
+    @Override
+    public void connectionLost(Throwable cause) {
+        Log.d(TAG, "Conexión perdida...");
+    }
+
+    @Override public void messageArrived(String topic, MqttMessage message)
+            throws Exception {
+        String payload = new String(message.getPayload()); Log.d(TAG, payload);
+        switch (payload) {
+            case "ON": mLedGpio.setValue(true); Log.d(TAG, "LED ON!"); break;
+            case "OFF": mLedGpio.setValue(false); Log.d(TAG, "LED OFF!"); break;
+            default:
+                Log.d(TAG, "Comando no soportado"); break;
+        } }
+    @Override public void deliveryComplete(IMqttDeliveryToken token) { Log.d(TAG, "Entrega completa!");
+    }
 }
